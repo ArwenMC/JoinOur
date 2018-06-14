4e35bd7de0bc0b6408572ef60b01ff3dcc6955fe
Christopher Nethercott <ccnethercott@gmail.com>
2018-06-14 17:14:57 +0100

    Create config.yml

--- src/main/java/com/arwenmc/JoinOur.java
+++ src/main/java/com/arwenmc/JoinOur.java
@@ -5,7 +5,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class JoinOur extends JavaPlugin {
 
     @Override
-    public void onEnable() {}
+    public void onEnable() {
+
+    }
 
     @Override
     public void onDisable() {}
