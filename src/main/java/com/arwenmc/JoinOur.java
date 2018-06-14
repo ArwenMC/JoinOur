8e27ce256359ea91b6d3eaa6f8307ec54d993a76
Christopher Nethercott <ccnethercott@gmail.com>
2018-06-14 17:07:00 +0100

    Added the basic JoinOur plugin class

---
+++ src/main/java/com/arwenmc/JoinOur.java
@@ -0,0 +1,13 @@
+package com.arwenmc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class JoinOur extends JavaPlugin {
+
+    @Override
+    public void onEnable() {}
+
+    @Override
+    public void onDisable() {}
+
+}
